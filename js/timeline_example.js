e8690005ffa7d5e04d3dfb5506b0480aee40f0eb
Nick Klauer <klauer@gmail.com>
2010-12-16 22:42:12 -0600

    Initial commit of an example Timeline setup.

---
+++ js/timeline_example.js
@@ -0,0 +1,37 @@
+var tl;
+function onLoad() {
+
+  var eventSource = new Timeline.DefaultEventSource();
+  var bandInfos = [
+    Timeline.createBandInfo({
+      eventSource:    eventSource,
+      width:          "70%",
+      intervalUnit:   Timeline.DateTime.MONTH,
+      intervalPixels: 100
+  }),
+    Timeline.createBandInfo({
+      eventSource:    eventSource,
+      width:          "30%",
+      intervalUnit:   Timeline.DateTime.YEAR,
+      intervalPixels: 200
+  })
+  ];
+
+  bandInfos[1].syncWith = 0;
+  bandInfos[1].highlight = true;
+
+  tl = Timeline.create(document.getElementById("my-timeline"), bandInfos);
+  Timeline.loadXML("/html/data.xml", function(xml, url) { eventSource.loadXML(xml,url); });
+}
+
+var resizeTimerId = null;
+function onResize() {
+  if (resizeTimerID == null) {
+    resizeTimerID = window.setTimeout(function () {
+      resizeTimerID = null;
+      tl.layout();
+    }, 500);
+  }
+}
+
+
